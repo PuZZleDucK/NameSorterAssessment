9b5b1569ecc93d1b7d3c718eed3eb5d69308727f
PuZZleDucK <puzzleduck@gmail.com>
2017-11-06 00:56:42 +1100

    add failing test for processing blank file

--- OpenPractice.Tests/Libs/NameSort/Integration/IntegrationTests.cs
+++ OpenPractice.Tests/Libs/NameSort/Integration/IntegrationTests.cs
@@ -8,14 +8,29 @@ namespace Integration
         [Fact]
         public void ProgramPrintsUsageWithoutArgs()
         {
-            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
-            pProcess.StartInfo.FileName = @"dotnet";
-            pProcess.StartInfo.Arguments = "run --project ../../../../../../../OpenPractice/Demos/name-sorter/";
-            pProcess.StartInfo.RedirectStandardOutput = true;
-            pProcess.Start();
-            string output = pProcess.StandardOutput.ReadToEnd();
-            pProcess.WaitForExit();
+            System.Diagnostics.Process name_sorter_proc = new System.Diagnostics.Process();
+            name_sorter_proc.StartInfo.FileName = @"../../../../../../../OpenPractice/Demos/name-sorter/bin/Release/netcoreapp2.0/ubuntu.14.04-x64/publish/name-sorter";
+            name_sorter_proc.StartInfo.RedirectStandardOutput = true;
+            name_sorter_proc.Start();
+            string output = name_sorter_proc.StandardOutput.ReadToEnd();
+            name_sorter_proc.WaitForExit();
             Assert.Matches("usage information", output);
+        }
+
+        [Fact]
+        public void ProgramProcessesEmptyFile()
+        {
+            System.Diagnostics.Process name_sorter_proc = new System.Diagnostics.Process();
+            name_sorter_proc.StartInfo.FileName = @"../../../../../../../OpenPractice/Demos/name-sorter/bin/Release/netcoreapp2.0/ubuntu.14.04-x64/publish/name-sorter";
+            string[] blank_line = { "" };
+            System.IO.File.WriteAllLines(@"blank.txt", blank_line);
+            name_sorter_proc.StartInfo.Arguments = "../../../../../../../OpenPractice.Tests/Libs/NameSort/Integration/bin/Debug/netcoreapp2.0/blank.txt";
+            name_sorter_proc.StartInfo.RedirectStandardOutput = true;
+            name_sorter_proc.Start();
+            string output = name_sorter_proc.StandardOutput.ReadToEnd();
+            name_sorter_proc.WaitForExit();
+            Assert.Equal("", output);
+            // read file output file assert too
         }
     }
 }
