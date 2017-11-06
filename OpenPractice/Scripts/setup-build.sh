5afc818990debed41140cd2987d3ecbe4b8c2f82
PuZZleDucK <puzzleduck@gmail.com>
2017-11-06 22:11:20 +1100

    remove unused build command

--- OpenPractice/Scripts/setup-build.sh
+++ OpenPractice/Scripts/setup-build.sh
@@ -5,4 +5,3 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-2.0.2
-dotnet publish -c release -r ubuntu.14.04-x64
