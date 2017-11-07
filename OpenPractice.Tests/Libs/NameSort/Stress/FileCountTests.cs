932d08b4d5c89b99093f88627e221b689792a201
PuZZleDucK <puzzleduck@gmail.com>
2017-11-07 18:36:55 +1100

    use corret test

--- OpenPractice.Tests/Libs/NameSort/Stress/FileCountTests.cs
+++ OpenPractice.Tests/Libs/NameSort/Stress/FileCountTests.cs
@@ -13,37 +13,23 @@ namespace Stress
             _name_sorter = SortBuilder.PrepareNameSorterProcess(_name_sorter);
         }
 
-       [Fact]
-        public void Handles1000NamesWithoutLoss()
-        {
-            string[] file_of_names = DataGenerator.Names(1000);
-            System.IO.File.WriteAllLines(@"1000.txt", file_of_names);
-            _name_sorter.StartInfo.Arguments = "1000.txt";
-            _name_sorter.Start();
-            string console_output = _name_sorter.StandardOutput.ReadToEnd();
-            _name_sorter.WaitForExit();
-            Assert.True(System.IO.File.Exists(@"sorted-names-list.txt"));
-            string[] output_file_lines = System.IO.File.ReadAllLines(@"sorted-names-list.txt");
-            Assert.Equal(file_of_names.Length, output_file_lines.Length);
-            Assert.Equal(String.Concat(file_of_names).Length, String.Concat(output_file_lines).Length);
-            System.IO.File.Delete(@"1000.txt");
-            System.IO.File.Delete(@"sorted-names-list.txt");
-        }
-
         [Fact]
-        public void Handles1000000NamesWithoutLoss()
+        public void Handles100FilesWithoutLoss()
         {
-            string[] file_of_names = DataGenerator.Names(1000000);
-            System.IO.File.WriteAllLines(@"1000000.txt", file_of_names);
-            _name_sorter.StartInfo.Arguments = "1000000.txt";
-            _name_sorter.Start();
-            string console_output = _name_sorter.StandardOutput.ReadToEnd();
-            _name_sorter.WaitForExit();
-            Assert.True(System.IO.File.Exists(@"sorted-names-list.txt"));
-            string[] output_file_lines = System.IO.File.ReadAllLines(@"sorted-names-list.txt");
-            Assert.Equal(file_of_names.Length, output_file_lines.Length);
-            Assert.Equal(String.Concat(file_of_names).Length, String.Concat(output_file_lines).Length);
-            System.IO.File.Delete(@"1000000.txt");
+            for(var file_count = 0; file_count < 100; file_count++)
+            {
+                string[] file_of_names = DataGenerator.Names(1000);
+                System.IO.File.WriteAllLines(@"1000.txt", file_of_names);
+                _name_sorter.StartInfo.Arguments = "1000.txt";
+                _name_sorter.Start();
+                string console_output = _name_sorter.StandardOutput.ReadToEnd();
+                _name_sorter.WaitForExit();
+                Assert.True(System.IO.File.Exists(@"sorted-names-list.txt"));
+                string[] output_file_lines = System.IO.File.ReadAllLines(@"sorted-names-list.txt");
+                Assert.Equal(file_of_names.Length, output_file_lines.Length);
+                Assert.Equal(String.Concat(file_of_names).Length, String.Concat(output_file_lines).Length);
+                System.IO.File.Delete(@"1000.txt");
+            }
             System.IO.File.Delete(@"sorted-names-list.txt");
         }
     }
